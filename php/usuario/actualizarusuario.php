f050926c991c9c11b78d70b816dbbb293d3cf4bc
yo <ferreironisiilucila@gmail.com>
2025-09-10 17:27:14 -0300

    j
    j

---
+++ php/usuario/actualizarusuario.php
@@ -0,0 +1,33 @@
+<?php
+require ('../conexion.php');
+session_start();
+
+if ($_SESSION['idusuario']) {
+        // Validar
+        $id = $_SESSION['idusuario'];
+        $nombre = mysqli_real_escape_string ($conexion,$_POST['nombre']);
+        $apellido = mysqli_real_escape_string ($conexion,$_POST['apellido']);
+        $dni = mysqli_real_escape_string ($conexion,$_POST['dni']);
+        $telefono= mysqli_real_escape_string ($conexion,$_POST['telefono']);
+        $obrasocial = mysqli_real_escape_string ($conexion,$_POST['obra_social']);
+        $correo = mysqli_real_escape_string ($conexion,$_POST['email']);
+        $idusuario = $_SESSION['idusuario'];
+
+        $sql = "UPDATE usuarios SET
+        nombre = '".$nombre."',
+        apellido = '".$apellido."',
+        dni = '".$dni."',
+        telefono = '".$telefono."',
+        obra_social = '".$obrasocial."',
+        email = '".$correo."'
+        WHERE id_usuario = '".$id."'";
+
+        $resultado = mysqli_query($conexion, $sql);
+
+        if ($resultado) {
+         header("Location: perfil.php");
+        } else {
+         echo "algo salio mal";
+        }
+    }
+?>
