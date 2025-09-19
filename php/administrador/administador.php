b89a2ba0169ea95625ee82724963b6f3335facce
juanmanuel-sys <juanmanuelgouzou@gmail.com>
2025-09-19 17:00:47 -0300

    Administrador
    administrador

--- php/administrador/administador.php
+++ php/administrador/administador.php
@@ -1,12 +1,28 @@
-  //aca consultamos el estado del turno, en caso de estar reservado (true) no imprimimos el boton y dejamos su fondo en rojo
-
-if ($array["estado"] ) {
-            //reservado
-            echo "<td id='fondo_reserva_reservado'>Turno Reservado</td>";
-        } else {
-            //no reservado
-            echo "<td id='fondo_reserva_disponible'><form action='reservar.php' method='post'>
-                        <input hidden type='number' name='id_turno' value='".$array["id_turno"]."'>
-                        <input type='submit' value='Reservar'>
-                </form></td>";
-        }
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../../css/administrador.css">
+  <title>Administrador</title>
+ </head>
+ <body>
+  
+ </body>
+ </html>
+ <table>
+        <tr>
+            <?php 
+            if ($array["estado"]) { //reservado
+                echo "<td id='fondo_reserva_reservado'>Turno Reservado</td>";
+            } else { //no reservado
+                echo "<td id='fondo_reserva_disponible'>
+                        <form action='reservar.php' method='post'>
+                            <input hidden type='number' name='id_turno' value='".$array["id_turno"]."'>
+                            <input type='submit' value='Reservar'>
+                        </form>
+                      </td>";
+            }
+            ?>
+        </tr>
+    </table>
