b5c1672e1a939f99837feb8643cf4168a3720192
ChocolatadaDeCenizas <t0t0.18.02.2007@gmail.com>
2025-09-22 15:16:30 -0300

    la bd

--- bd/saludigital.sql
+++ bd/saludigital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2025 a las 15:04:51
+-- Tiempo de generación: 22-09-2025 a las 20:14:54
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -30,10 +30,23 @@ SET time_zone = "+00:00";
 CREATE TABLE `horarios` (
   `id_horario` int(11) NOT NULL,
   `id_profesional` int(11) NOT NULL,
-  `id_servicio` int(11) NOT NULL,
   `dia` date NOT NULL,
   `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `horarios`
+--
+
+INSERT INTO `horarios` (`id_horario`, `id_profesional`, `dia`, `hora`) VALUES
+(1, 4, '2025-09-22', '10:00:00'),
+(2, 4, '2025-09-23', '11:00:00'),
+(3, 5, '2025-09-22', '12:00:00'),
+(4, 5, '2025-09-23', '13:00:00'),
+(5, 6, '2025-09-22', '14:00:00'),
+(6, 6, '2025-09-24', '15:00:00'),
+(7, 7, '2025-09-22', '16:00:00'),
+(8, 7, '2025-09-24', '17:00:00');
 
 -- --------------------------------------------------------
 
@@ -52,6 +65,16 @@ CREATE TABLE `profesionales` (
   `intervalo` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `profesionales`
+--
+
+INSERT INTO `profesionales` (`id_profesional`, `nombre`, `apellido`, `id_servicio`, `dias_trabajados`, `hora_inicio`, `hora_fin`, `intervalo`) VALUES
+(4, 'Karina', 'Latorre', 5, 'lunes,miercoles,viernes', '12:00:00', '18:00:00', 30),
+(5, 'Julian', 'Delano', 3, 'miercoles,jueves,viernes', '08:00:00', '16:00:00', 30),
+(6, 'Cristofer', 'Estrada', 4, 'martes,jueves', '14:00:00', '20:00:00', 20),
+(7, 'Valentina', 'Pincharrata', 3, 'martes,jueves,viernes', '09:00:00', '17:00:00', 30);
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +86,15 @@ CREATE TABLE `servicios` (
   `nombre` varchar(30) NOT NULL,
   `descripcion` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `servicios`
+--
+
+INSERT INTO `servicios` (`id_servicio`, `nombre`, `descripcion`) VALUES
+(3, 'cardiologia', 'del cocoro'),
+(4, 'dermatologia', 'de la pielcita'),
+(5, 'fonoaudiologia', 'del habla');
 
 -- --------------------------------------------------------
 
@@ -76,6 +108,16 @@ CREATE TABLE `turnos` (
   `id_horario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `turnos`
+--
+
+INSERT INTO `turnos` (`id_turno`, `id_usuario`, `id_horario`) VALUES
+(6, 12, 5),
+(7, 12, 2),
+(8, 12, 4),
+(9, 12, 7);
+
 -- --------------------------------------------------------
 
 --
@@ -87,12 +129,23 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(30) NOT NULL,
   `dni` int(8) NOT NULL,
-  `telefono` int(20) NOT NULL,
+  `telefono` bigint(20) NOT NULL,
   `obra_social` varchar(30) NOT NULL,
   `email` varchar(30) NOT NULL,
   `contrasenia` varchar(255) NOT NULL,
   `rol` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `dni`, `telefono`, `obra_social`, `email`, `contrasenia`, `rol`) VALUES
+(9, 'toto', 'coto', 27164745, 0, 'dar salud', 'tomamediarri@gmail.com', 'eb1dd5ee0087e5012f24ae089324b20f', 0),
+(10, 'qsy', 'wacho', 243424234, 0, 'papafrita', 'juan@juan', '81dc9bdb52d04dc20036dbd8313ed055', 0),
+(12, 'gige', 'gige', 163781627, 22621234, 'papafrita', 'mati@mati', '81dc9bdb52d04dc20036dbd8313ed055', 0),
+(13, 'fulano', 'mengano', 12345432, 12321312, 'pipo gorosito', 'totute@pedo', '81dc9bdb52d04dc20036dbd8313ed055', 0),
+(14, 'ayuya', 'wokiikow', 23232323, 3232324, 'mecagoencima', 'penepene@pene', '81dc9bdb52d04dc20036dbd8313ed055', 0);
 
 --
 -- Índices para tablas volcadas
@@ -103,8 +156,7 @@ CREATE TABLE `usuarios` (
 --
 ALTER TABLE `horarios`
   ADD PRIMARY KEY (`id_horario`),
-  ADD KEY `id_profesional` (`id_profesional`),
-  ADD KEY `id_servicio` (`id_servicio`);
+  ADD KEY `id_profesional` (`id_profesional`);
 
 --
 -- Indices de la tabla `profesionales`
@@ -141,31 +193,31 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `profesionales`
 --
 ALTER TABLE `profesionales`
-  MODIFY `id_profesional` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_profesional` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_turno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Restricciones para tablas volcadas
@@ -175,8 +227,7 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  ADD CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`id_profesional`) REFERENCES `profesionales` (`id_profesional`),
-  ADD CONSTRAINT `horarios_ibfk_2` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicio`);
+  ADD CONSTRAINT `horarios_ibfk_1` FOREIGN KEY (`id_profesional`) REFERENCES `profesionales` (`id_profesional`);
 
 --
 -- Filtros para la tabla `profesionales`
