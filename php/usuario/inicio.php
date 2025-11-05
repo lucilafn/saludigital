af6877372b3df1ea6e1936bfe3dcdc1cf1d22881
ChocolatadaDeCenizas <t0t0.18.02.2007@gmail.com>
2025-11-05 17:15:44 -0300

    ujhfhjdi
    sahfdhagdu

--- php/usuario/inicio.php
+++ php/usuario/inicio.php
@@ -51,29 +51,22 @@
       <span>SaluDigital</span>
     </div>
     <nav>
-        <div class="acciones">
-          <?php if (!isset($_SESSION['usuario'])): ?>
-              <a href="form_iniciosesion.php"><img src="../../img/login.png"> Iniciar sesión</a>
-              <a href="form_registro.php"><img src="../../img/register.png"> Registrarse</a>
-          <?php else: ?>
-              <a href="perfil.php"><img src="../../img/profile.png"> Mi Perfil</a>
-              <a href="../turnos/pedirturno.php"><img src="../../img/calendar.png"> Turnos</a>
-              <a href="cerrarsesion.php"><img src="../../img/logout.png"> Cerrar sesión</a>
-          <?php endif; ?>
-      </div>
-    </nav>
-    <!-- Top Navigation Menu -->
-  <div class="topnav">
+    <div class="topnav">
     <div id="myLinks">
-      <a href="index.html" class="active">Inicio</a>
-      <a href="php/nosotros/servicios.html">Servicios</a>
-      <a href="php/nosotros/nosotros.html">Nosotros</a>
-      <a href="php/nosotros/contacto.html">Contacto</a>
-    </div>
+          <?php if (!isset($_SESSION['usuario'])): ?>
+              <a href="form_iniciosesion.php"> Iniciar sesión</a>
+              <a href="form_registro.php">Registrarse</a>
+          <?php else: ?>
+              <a href="perfil.php"> Mi Perfil</a>
+              <a href="../turnos/pedirturno.php"> Turnos</a>
+              <a href="cerrarsesion.php">Cerrar sesión</a>
+          <?php endif; ?>
+           </div>
       <a href="javascript:void(0);" class="icon" onclick="myFunction()">
         <i class="fa fa-bars"></i>
       </a>
   </div>
+    </nav>
   </header>
 
   <!-- ===== SECCIÓN BIENVENIDA ===== -->
