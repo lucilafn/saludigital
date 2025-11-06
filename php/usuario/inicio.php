37252fb9be55c76d48ac8f05a730db367ccc3539
juanmanuel-sys <juanmanuelgouzou@gmail.com>
2025-11-05 23:01:13 -0300

    klklklk
    123445

--- php/usuario/inicio.php
+++ php/usuario/inicio.php
@@ -12,36 +12,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../../css/inicio.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <title>Inicio</title>
   </head>
   <body>
     <header>
       <div class="logo">
-        <img src="img/logo.png" alt="Logo Consultorio" width="35">
+      <img src="../../img/logosdl.png" alt="">
         <span>SaluDigital</span>
       </div>
-      <nav>
-        <div class="topnav">
-          <div id="myLinks">
-            <?php if (!isset($_SESSION['usuario'])): ?>
-              <a href="form_iniciosesion.php"> Iniciar sesión</a>
-              <a href="form_registro.php">Registrarse</a>
-            <?php else: ?>
-              <a href="perfil.php"> Mi Perfil</a>
-              <a href="../turnos/pedirturno.php"> Turnos</a>
-              <a href="cerrarsesion.php">Cerrar sesión</a>
-            <?php endif; ?>
-          </div>
-          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-            <i class="fa fa-bars"></i>☰
-          </a>
-          <script>
-            function toggleMenu() {
-            document.getElementById("nav").classList.toggle("show");
-s</script>
-        </div>
-      </nav>
+      <nav class="topnav">
+    <div id="myLinks">
+      <?php if (!isset($_SESSION['usuario'])): ?>
+        <a href="form_inicioSesion.php">Iniciar sesión</a>
+        <a href="form_registro.php">Registrarse</a>
+      <?php else: ?>
+        <a href="perfil.php">Mi Perfil</a>
+        <a href="../turnos/pedirturno.php">Turnos</a>
+        <a href="cerrarsesion.php">Cerrar sesión</a>
+      <?php endif; ?>
+    </div>
+  </nav>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+      <i class="fa fa-bars"></i>
+    </a>
     </header>
+    <script>
+function myFunction() {
+  document.getElementById("myLinks").classList.toggle("show");
+}
+</script>
     <!-- ===== SECCIÓN BIENVENIDA ===== -->
     <div class="seccion">
       <h1>Bienvenido</h1>
@@ -77,5 +77,15 @@ s</script>
     <footer>
       <p>Información de nuestro emprendimiento</p>
     </footer>
+    <script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
   </body>
 </html>
